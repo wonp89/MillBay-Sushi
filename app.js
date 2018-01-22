c851fb05479f2c40e9142d45231580a89d2870f1
Won <wonpark89@gmail.com>
2018-01-22 12:23:02 -0800

    modified app.js

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ var menu = require('./routes/menu');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,11 +21,16 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'mill-bay-sushi-react/build')))
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(menu);
 app.use(contact);
 // app.use('/users', users);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/mill-bay-sushi-react/build/index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
