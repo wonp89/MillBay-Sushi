2bff09e9e6e080a75476c77ba93572f608539bf9
Won <wonpark89@gmail.com>
2018-02-04 15:22:49 -0800

    recommit after app.js fixed

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'mill-bay-sushi-react/public')))
-// app.use(express.static(path.join(__dirname, 'mill-bay-sushi-react/build')))
+// app.use(express.static(path.join(__dirname, 'mill-bay-sushi-react/public')))
+app.use(express.static(path.join(__dirname, 'mill-bay-sushi-react/build')))
 app.use(menu);
 app.use(contact);
 
