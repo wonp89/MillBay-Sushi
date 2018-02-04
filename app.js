27e19546a45a4855e7ed513c954a494283c3adb8
Won <wonpark89@gmail.com>
2018-02-04 14:55:46 -0800

    for heroku deployment

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'mill-bay-sushi-react/public')))
+// app.use(express.static(path.join(__dirname, 'mill-bay-sushi-react/public')))
 app.use(express.static(path.join(__dirname, 'mill-bay-sushi-react/build')))
 app.use(menu);
 app.use(contact);
