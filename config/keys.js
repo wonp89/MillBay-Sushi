8f87a030abaefed31e32281514a49f1dd5f25706
Won <wonpark89@gmail.com>
2018-02-05 00:55:29 -0800

    background pattern added for deployment

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  googleClientID: "470435395869-534s8u3hg31b4n60vcqi95he6sv6g6vn.apps.googleusercontent.com",
-  googleClientSecret: "KgTRDiMYlkZTqPdWUDFVlm-N",
-  refreshToken: "1/omQaq8Yb0uReUn6Rvfm45wQlV41C5Hbv_7lF65f7rbo",
-  accessToken: "ya29.GltFBUrTvNRMfTBa3YqiptIwdgJRDDo2xUVokod4ndHN9pYFExxjxbeBld_fWVNJx7GCd0igjGWhOY75R3iDeIwF8RLv00ua3uFNhJGzqglGFRFtFgYzbRp77Lla"
-}
