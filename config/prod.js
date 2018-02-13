de0c7943e376d0ddb6d9ffe0ff3bc8c24d3541a6
Won <wonpark89@gmail.com>
2018-02-12 17:13:50 -0800

    setting up keys for prod

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  refreshToken: process.env.REFRESH_TOKEN,
+  accessToken: process.env.ACCESS_TOKEN
+};
