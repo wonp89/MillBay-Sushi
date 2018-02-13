8ddcbff99622271361043b4542cd47f139307a44
Won <wonpark89@gmail.com>
2018-02-12 23:45:35 -0800

    heroku deployment with bamboo image

--- mill-bay-sushi-react/src/components/Intro.js
+++ mill-bay-sushi-react/src/components/Intro.js
@@ -28,7 +28,7 @@ class Intro extends Component {
                 </Scroll>
               </div>
           </div>
-          <img src="http://localhost:3000/img/bamboo.png" alt="" />
+          <img src="http://millbaysushi.com/img/bamboo.png" alt="" />
         <div id={classes.IntroImages}>
           <InteriorPhotos />
         </div>
