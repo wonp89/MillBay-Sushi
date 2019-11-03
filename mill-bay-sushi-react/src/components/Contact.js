11dd3c151b739b728c72bc6d0be0a87c2ccf4d61
Won Teak Park <wonpark@Wons-MacBook-Air.local>
2019-11-03 01:56:29 -0800

    changing phone number

--- mill-bay-sushi-react/src/components/Contact.js
+++ mill-bay-sushi-react/src/components/Contact.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Map from './container/Map'
 import classes from './css/Contact.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
@@ -6,9 +6,9 @@ import RaisedButton from 'material-ui/RaisedButton';
 import WOW from 'wowjs';
 
 class Contact extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
-    this.state = {sent: false}
+    this.state = { sent: false }
     this.onSubmit = this.contact.bind(this);
   }
 
@@ -16,7 +16,7 @@ class Contact extends Component {
     new WOW.WOW().init();
   }
 
-  contact (e) {
+  contact(e) {
     e.preventDefault()
     var self = this;
     return fetch(
@@ -35,11 +35,11 @@ class Contact extends Component {
         })
       }
     )
-    .then(() => {
-          const sentBoolean = this.state.sent;
-          this.setState({sent: !sentBoolean});
-          })
-    .catch(error => alert("Error occured while sending your message"))
+      .then(() => {
+        const sentBoolean = this.state.sent;
+        this.setState({ sent: !sentBoolean });
+      })
+      .catch(error => alert("Error occured while sending your message"))
   }
 
   render() {
@@ -59,7 +59,7 @@ class Contact extends Component {
               <div className={classes.formField}>
                 <label htmlFor="subject">
                   <div className="label-content">Subject:</div>
-                  <input type="text" ref="subject"  required />
+                  <input type="text" ref="subject" required />
                 </label>
               </div>
               <div className={classes.formField}>
@@ -74,7 +74,7 @@ class Contact extends Component {
                   <input type="text" ref="from" required />
                 </label>
               </div>
-              <div className={classes.formField} style={this.state.sent === false ? {borderBottom: "none"} : {borderBottom: "solid rgb(100, 216, 242)"}} >
+              <div className={classes.formField} style={this.state.sent === false ? { borderBottom: "none" } : { borderBottom: "solid rgb(100, 216, 242)" }} >
                 <label htmlFor="text">
                   <div className="label-content">Message:</div>
                   <textarea className="stretch" ref="text" rows="5" required />
@@ -82,18 +82,18 @@ class Contact extends Component {
               </div>
               {button}
             </form>
-            <div id={classes.contactHeadingContent} style={this.state.sent === false ? {border: "none"} : {border: "solid rgb(100, 216, 242)"}} >
-                  <h1 className="wow fadeInUp"><a href="tel:+2509297117" id={classes.phoneNumber}> 250. 929.  7117</a></h1>
-                  <p style={{animationDelay: '0.2s'}} className="wow fadeInUp"><span>Email</span> millbay.sushi@gmail.com</p>
-                  <p style={{animationDelay: '0.3s'}} className="wow fadeInUp"><span>Address</span> 2720 Mill bay RD, Mill bay, BC, V0R 2P1</p>
-                  <p style={{animationDelay: '0.4s'}} className="wow fadeInUp"><span>Hours</span> Open 7 days a week 11:30AM ~ 9:00PM</p>
+            <div id={classes.contactHeadingContent} style={this.state.sent === false ? { border: "none" } : { border: "solid rgb(100, 216, 242)" }} >
+              <h1 className="wow fadeInUp"><a href="tel:+12509297117" id={classes.phoneNumber}> 250. 929.  7117</a></h1>
+              <p style={{ animationDelay: '0.2s' }} className="wow fadeInUp"><span>Email</span> millbay.sushi@gmail.com</p>
+              <p style={{ animationDelay: '0.3s' }} className="wow fadeInUp"><span>Address</span> 2720 Mill bay RD, Mill bay, BC, V0R 2P1</p>
+              <p style={{ animationDelay: '0.4s' }} className="wow fadeInUp"><span>Hours</span> Open 7 days a week 11:30AM ~ 9:00PM</p>
             </div>
           </div>
-            <Map googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBOO071sg9pRmhLo_iBA0ycoKczwmMmfyA&v=3.exp&libraries=geometry,drawing,places"
+          <Map googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBOO071sg9pRmhLo_iBA0ycoKczwmMmfyA&v=3.exp&libraries=geometry,drawing,places"
             loadingElement={<div id={classes.mapLoadingElement} />}
             containerElement={<div id={classes.mapContainerElement} />}
             mapElement={<div id={classes.mapElement} />}
-            />
+          />
         </div>
         <center className={classes.copyRight}>Copyright © 2018 by Mill Bay Sushi  All Rights Reserved. Website created by: wonpark89@gmail.com</center>
       </MuiThemeProvider>
