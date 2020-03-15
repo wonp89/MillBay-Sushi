fb21b812a053e8829e80db69a6d0baae30ce75a3
Won Teak Park <wonpark@Wons-MacBook-Air.local>
2020-03-15 09:12:40 -0700

    togo menu link has been removed

--- mill-bay-sushi-react/src/components/Menu.js
+++ mill-bay-sushi-react/src/components/Menu.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import TopSlideImages from './container/TopSlidePhotos'
 import Scroll from 'react-scroll-to-element';
 import Intro from './Intro';
@@ -12,7 +12,7 @@ import { ParallaxProvider } from 'react-scroll-parallax';
 import WOW from 'wowjs';
 
 class Menu extends Component {
-  state = {menu: []};
+  state = { menu: [] };
   componentDidMount() {
     new WOW.WOW().init();
     fetch('/menu')
@@ -20,76 +20,73 @@ class Menu extends Component {
       .then(menu => this.setState({ menu: menu }));
   }
 
-//boolean for displaying menus
+  //boolean for displaying menus
   showMenu = (id) => {
-    const clickedMenu = {...this.state.menu[id]};
+    const clickedMenu = { ...this.state.menu[id] };
     const show = clickedMenu.show
     const menus = [...this.state.menu]
     menus[id].show = !show
-    this.setState({menu: menus})
-    menus[id].show  === true ? document.body.style.overflow = 'hidden' : document.body.style.overflow = 'scroll';
+    this.setState({ menu: menus })
+    menus[id].show === true ? document.body.style.overflow = 'hidden' : document.body.style.overflow = 'scroll';
   }
 
-//pressing button goes to the next pages.
+  //pressing button goes to the next pages.
   nextMenu = (id) => {
     const menus = [...this.state.menu]
     menus[id].show = false;
     let nextPage = menus[id + 1]
     if (nextPage === undefined) nextPage = menus[0]
     nextPage.show = !nextPage.show
-    this.setState({menu: menus})
+    this.setState({ menu: menus })
   }
 
-//pressing button goes to the previous pages.
+  //pressing button goes to the previous pages.
   previousMenu = (id) => {
     const menus = [...this.state.menu]
     menus[id].show = false;
     let nextPage = menus[id - 1]
     if (nextPage === undefined) nextPage = menus[16]
     nextPage.show = !nextPage.show
-    this.setState({menu: menus})
+    this.setState({ menu: menus })
   }
 
-    render() {
+  render() {
 
-      return (
-          <div id="Menu">
-            <Scroll>
-              <div id={classes.scrollTop}>⇪</div>
-            </Scroll>
-            <Intro />
-            <TopSlideImages />
-            <div id={classes.AllTypes} style={{backgroundImage: 'url(http://www.millbaysushi.com/img/pw_pattern.png)'}}>
-                <div id={classes.menuHeading} className="wow slideInLeft">
-                  <h1>MENU</h1>
-                </div>
-                {this.state.menu.map( (list, index) =>
-                   <MenuPanels key={index} click={() => this.showMenu(list.id)} name={list.name} index={index} />
-                )}
-            </div>
-            <div id={classes.menuContent} style={{backgroundImage: 'url(http://www.millbaysushi.com/img/pw_pattern.png)'}}>
-              <a href="http://www.millbaysushi.com/img/MillBaySushi-TogoMenu.pdf" style={{marginBottom: '0'}}  rel="noopener noreferrer" target="_blank">
-                <span>⚬○. TAKE OUT MENU .○⚬</span>
-              </a>
-            </div>
-            <div style={{maxWidth: '1440px', overflow: 'hidden', paddingBottom: '30px'}}>
-              <img src="http://www.millbaysushi.com/img/cloud.png" style={{position: 'sticky', zIndex: '-1'}} alt="" />
-            </div>
-            <Hours className='wow fadeIn'/>
-            <div>
-              {this.state.menu.map((types, index)  =>
-                {if (types.show === true)
-                    return <TypesOfFood foodType={types.name} key={index} types={types.food} click={() => this.showMenu(types.id)} next={() => this.nextMenu(types.id)} previous={() => this.previousMenu(types.id)} />
-                }
-              )}
-            </div>
-            <ParallaxProvider>
-              <SalmonPainting2 />
-            </ParallaxProvider>
-            <Ocean />
+    return (
+      <div id="Menu">
+        <Scroll>
+          <div id={classes.scrollTop}>⇪</div>
+        </Scroll>
+        <Intro />
+        <TopSlideImages />
+        <div id={classes.AllTypes} style={{ backgroundImage: 'url(http://www.millbaysushi.com/img/pw_pattern.png)' }}>
+          <div id={classes.menuHeading} className="wow slideInLeft">
+            <h1>MENU</h1>
           </div>
-      );
-    }
+          {this.state.menu.map((list, index) =>
+            <MenuPanels key={index} click={() => this.showMenu(list.id)} name={list.name} index={index} />
+          )}
+        </div>
+        <div id={classes.menuContent} style={{ backgroundImage: 'url(http://www.millbaysushi.com/img/pw_pattern.png)' }}>
+        </div>
+        <div style={{ maxWidth: '1440px', overflow: 'hidden', paddingBottom: '30px' }}>
+          <img src="http://www.millbaysushi.com/img/cloud.png" style={{ position: 'sticky', zIndex: '-1' }} alt="" />
+        </div>
+        <Hours className='wow fadeIn' />
+        <div>
+          {this.state.menu.map((types, index) => {
+            if (types.show === true)
+              return <TypesOfFood foodType={types.name} key={index} types={types.food} click={() => this.showMenu(types.id)} next={() => this.nextMenu(types.id)} previous={() => this.previousMenu(types.id)} />
+          }
+          )}
+        </div>
+        <ParallaxProvider>
+          <SalmonPainting2 />
+        </ParallaxProvider>
+        <Ocean />
+      </div>
+    );
+  }
 
 }
 
